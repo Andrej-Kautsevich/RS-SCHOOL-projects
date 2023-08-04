a71a520d5bf3809319435b61a01f6e870792994d
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2023-08-04 23:46:39 +0300

    feat: add burger menu

--- library/index.js
+++ library/index.js
@@ -1,3 +1,7 @@
-console.log(
-  "Вёрстка валидная +10\nВёрстка семантическая +16\nВёрстка соответствует макету +54\nблок header +8\nсекция Welcome +4\nсекция About +6\nсекция Favorites +8\nсекция CoffeeShop +6\nсекция Contacts +6\nсекция LibraryCard +8\nблок footer +8\nОбщие требования к верстке +20\n"
-);
+const menuBtn = document.querySelector('.header__menu-btn');
+const menuBurger = document.querySelector('.header__menu');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('header__menu-btn_active');
+  menuBurger.classList.toggle('header__menu_open');
+});
