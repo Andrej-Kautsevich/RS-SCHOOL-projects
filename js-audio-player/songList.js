3d646592850b2a0924c3ce9911d8af8967155a8c
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2023-09-25 00:40:57 +0300

    feat: add music

--- js-audio-player/songList.js
+++ js-audio-player/songList.js
@@ -1,15 +1,33 @@
 const songsList = [
   {
-    name: "Don't Hurt Yourself",
-    artist: "Beyonce",
-    cover: "assets/img/lemonade.png",
-    audio: "assets/audio/beyonce.mp3",
+    name: "Dominant",
+    artist: "Rich Chamomile",
+    cover: "assets/img/Dominant.webp",
+    audio: "assets/audio/Rich Chamomile - Dominant.mp3",
   },
   {
-    name: "Don't Start Now",
-    artist: "Dua Lipa",
-    cover: "assets/img/dontstartnow.png",
-    audio: "assets/audio/dontstartnow.mp3",
+    name: "Хардкор",
+    artist: "Прости, Герда!",
+    cover: "assets/img/Sorry Gerda.jpg",
+    audio: "assets/audio/Прости, Герда! - Хардкор.mp3",
+  },
+  {
+    name: "Зоры загiнуць",
+    artist: "Fishtank",
+    cover: "assets/img/Fishtank.webp",
+    audio: "assets/audio/fishtank - Зоры загiнуць.mp3",
+  },
+  {
+    name: "видеть тебя",
+    artist: "Surf Punk",
+    cover: "assets/img/Surf Punk.webp",
+    audio: "assets/audio/surf punk - видеть тебя.mp3",
+  },
+  {
+    name: "детство",
+    artist: "стой!стреляют",
+    cover: "assets/img/a9Cgz0cZeZM.jpg",
+    audio: "assets/audio/стой!стреляют - детство.mp3",
   },
 ]
 
