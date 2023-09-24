7bac66f64dc4a8ea5095c5c4e08003111f381b18
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2023-09-24 13:44:14 +0300

    refactor: move songs list to a new file

--- js-audio-player/index.js
+++ js-audio-player/index.js
@@ -4,20 +4,7 @@ let songIndex = 0;
 let isPlay = false;
 
 //Songs list
-const songsList = [
-  {
-    name: "Don't Hurt Yourself",
-    artist: "Beyonce",
-    cover: "assets/img/lemonade.png",
-    audio: "assets/audio/beyonce.mp3",
-  },
-  {
-    name: "Don't Start Now",
-    artist: "Dua Lipa",
-    cover: "assets/img/dontstartnow.png",
-    audio: "assets/audio/dontstartnow.mp3",
-  },
-]
+import songsList from "./songList.js";
 
 //Start and stop music
 playBtn.addEventListener('click', () => {
