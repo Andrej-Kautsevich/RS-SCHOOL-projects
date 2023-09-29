92d739f9b8ab58a9e9a51d1ceee6fb63a93ea03f
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2023-09-29 14:36:18 +0300

    feat: implement request to API

---
+++ image-galery/index.js
@@ -0,0 +1,30 @@
+const url = 'https://api.unsplash.com/photos/?client_id=WsxvXatvQCJuWcNE8EtNMRIKN8Ym6IB_zau0qKUDAN8'
+
+
+
+
+async function getData() {
+	const res = await fetch(url);
+	const data = await res.json();
+	console.log(data);
+	showImages(data);
+}
+getData();
+
+function showData(data) {
+
+}
+
+
+//generate DOMelements
+const container = document.querySelector('.container');
+
+function showImages(images) {
+	images.map((imageObj) => {
+		const img = document.createElement('img');
+		img.classList.add('gallery-img')
+		img.src = imageObj.urls.regular;
+		img.alt = `image`;
+		container.append(img);
+	})
+}
