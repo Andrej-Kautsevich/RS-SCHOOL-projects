ca8fee43032982a4d1f0125a0001a1c9b41f3e1f
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2023-10-01 21:44:04 +0300

    feat: add photo card pop-up

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,19 +1,16 @@
-let url = new URL("https://api.unsplash.com")
-let params = new URLSearchParams(url.search); //api request parameters
-let request
 const apiKey = 'WsxvXatvQCJuWcNE8EtNMRIKN8Ym6IB_zau0qKUDAN8'
-params.set("client_id", apiKey);
-url.search = params.toString();
+const overlay = document.querySelector('.overlay');
+
+let request
 
 async function apiRequest(request) {
-	//if there are no request, show random photos
+	let url;
 	if (request !== undefined) {
-		params.set("query", request);
-		url.search = params.toString();
-		url.pathname = "/search/photos";
+		url = `https://api.unsplash.com/search/photos?query=${request}&client_id=${apiKey}`;
 	} else {
-		url.pathname = "/photos";
+		url = `https://api.unsplash.com/photos?client_id=${apiKey}`;
 	}
+
 	try {
 		const res = await fetch(url);
 		const data = await res.json();
@@ -22,12 +19,64 @@ async function apiRequest(request) {
 		} else {
 			showImages(data);
 		}
-		console.log(data, url)
+		console.log(data)
 	} catch (error) {
 		alert("Server is not response");
 	}
 }
 apiRequest();
+
+async function apiRequestPhoto(id) {
+	try {
+		const url = `https://api.unsplash.com/photos/${id}?client_id=${apiKey}`;
+		const res = await fetch(url);
+		const stats = await res.json();
+
+		updateImageCard(stats);
+
+		console.log(stats, url)
+	} catch (error) {
+		console.error("Error:", error);
+	}
+}
+
+
+function showImageCard() {
+	const id = this.dataset.id;
+	overlay.classList.add('overlay_visible')
+	apiRequestPhoto(id);
+
+	console.log(id)
+}
+
+//close imageCard
+overlay.addEventListener('click', (e) => {
+	if (e.target.classList.contains('overlay')) {
+		overlay.classList.remove('overlay_visible');
+		const photo = document.querySelector('.card__image');
+		photo.setAttribute('src', "");
+	}
+})
+
+function updateImageCard(image) {
+	const userLink = document.querySelector('.card__user-link');
+	const userAvatar = document.querySelector('.card__user-avatar');
+	const userName = document.querySelector('.card__user-name');
+	const download = document.querySelector('.card__download');
+	const views = document.getElementById('views');
+	const downloads = document.getElementById('downloads');
+	const likes = document.getElementById('likes');
+	const photo = document.querySelector('.card__image');
+
+	userLink.setAttribute('href', image.user.links.html);
+	userAvatar.setAttribute('src', image.user.profile_image.medium);
+	userName.innerText = image.user.name;
+	views.innerText = image.views;
+	downloads.innerText = image.downloads;
+	likes.innerText = image.likes;
+	photo.setAttribute('src', image.urls.regular);
+	download.dataset.download = image.urls.full;
+}
 
 const searchInput = document.querySelector('.search');
 
@@ -38,6 +87,24 @@ searchInput.addEventListener('keydown', (e) => {
 	}
 })
 
+function downloadImage(url) {
+	fetch(url).then((res) => {
+		res.blob().then((img) => {
+			const a = document.createElement('a');
+			a.href = URL.createObjectURL(img);
+			a.download = 'file';
+			a.click();
+		});
+	}).catch((error) => {
+		console.log('Error:', error);
+	});
+}
+document.querySelector(".card__download").addEventListener("click", function(event) {
+	const url = this.getAttribute("data-download");
+	downloadImage(url);
+});
+
+
 //generate DOMelements
 const gallery = document.querySelector('.gallery');
 
@@ -46,10 +113,15 @@ function showImages(images) {
 	images.map((imageObj) => {
 		const img = document.createElement('img');
 		img.classList.add('gallery-img')
-		img.src = imageObj.urls.regular;
+		img.src = imageObj.urls.small;
 		img.alt = `image`;
+		img.dataset.id = imageObj.id;
+		img.onclick = showImageCard;
 		gallery.append(img);
 	})
 }
+
+
+
 
 document.addEventListener("DOMContentLoaded", () => searchInput.focus());
