53cb5124bdb80f8005e1c5fd46ca3f89f683a521
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2023-12-11 01:52:46 +0300

    feat: add changing window size on menu page

--- coffee-house/src/script/menu.js
+++ coffee-house/src/script/menu.js
@@ -4,9 +4,13 @@ const res = await fetch('src/products.json');
 const data = await res.json();
 
 const menuItemsContainer = document.querySelector('.menu__items');
+const menuAddBtn = document.querySelector('.menu__add-button')
+
+const DESKTOP_WIDTH = 1024;
 
 let currentItems = [];
 let currentCategory = 'coffee';
+let isDesktop = true;
 
 function getItems() {
   currentItems = data.filter((item) => item.category === currentCategory);
@@ -22,13 +26,66 @@ function createItem(item, index) {
   menuItemsContainer.appendChild(product)
 }
 
-let visibleItemsCount = 4;
+let visibleItemsCount = currentItems.length;
 
-function renderItems(itemsCount) {
-  currentItems.forEach((item, index) => {
-    if (index < itemsCount) { createItem(item, index); }
-  })
+function renderItems(startIndex, endIndex) {
+  for (let index = startIndex; index < endIndex; index++) {
+    let item = currentItems[index];
+    if (item) { createItem(item, index); }
+  }
+
+  if (visibleItemsCount >= currentItems.length) {
+    menuAddBtn.style.display = 'none';
+  }
 }
 
+function setVisibleItemsCount() {
+  if (window.innerWidth < DESKTOP_WIDTH) {
+    isDesktop = false;
+    visibleItemsCount = 4;
+    if (visibleItemsCount < currentItems.length) {
+      menuAddBtn.style.display = 'flex';
+    }
+  }
+}
 
-renderItems(visibleItemsCount);
+function loadMoreItems() {
+  let startIndex = visibleItemsCount;
+  let endIndex = startIndex + 4;
+  visibleItemsCount += 4;
+  renderItems(startIndex, endIndex);
+}
+
+menuAddBtn.addEventListener('click', () => {
+  loadMoreItems()
+})
+
+
+setVisibleItemsCount();
+renderItems(0, visibleItemsCount);
+
+window.addEventListener('resize', () => {
+  //change from desktop to mobile
+  if (isDesktop && window.innerWidth < DESKTOP_WIDTH) {
+    console.log('change')
+    isDesktop = false;
+    visibleItemsCount = 4;
+
+    menuItemsContainer.innerHTML = '';
+    if (visibleItemsCount < currentItems.length) {
+      menuAddBtn.style.display = 'flex';
+    }
+    renderItems(0, visibleItemsCount);
+  }
+
+  //change from mobile to desktop
+  if (!isDesktop && window.innerWidth >= DESKTOP_WIDTH) {
+    isDesktop = true;
+    if (visibleItemsCount < currentItems.length) {
+      renderItems(visibleItemsCount, currentItems.length);
+    }
+    if (visibleItemsCount >= currentItems.length) {
+      menuAddBtn.style.display = 'none';
+    }
+  }
+})
