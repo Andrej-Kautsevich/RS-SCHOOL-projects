b5c9c720a38a1479af5924169f956fc305b9e37b
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-01-14 23:22:48 +0300

    feat: update hangman appearance

--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -6,12 +6,12 @@ class Hangman {
     hangman.innerHTML = `
       <img class="hangman__gallows" src="assets/gallows.jpg" alt="" />
       <div class="hangman__man">
-        <img class="hangman__head" src="assets/head.svg" alt="" />
-        <img class="hangman__body" src="assets/body.svg" alt="" />
-        <img class="hangman__hand-left" src="assets/hand-left.svg" alt="" />
-        <img class="hangman__hand-right" src="assets/hand-right.svg" alt="" />
-        <img class="hangman__leg-left" src="assets/leg-left.svg" alt="" />
-        <img class="hangman__leg-right" src="assets/leg-right.svg" alt="" />
+        <img class="hangman__part hangman__part--head" src="assets/head.svg" alt="" />
+        <img class="hangman__part hangman__part--body" src="assets/body.svg" alt="" />
+        <img class="hangman__part hangman__part--hand-left" src="assets/hand-left.svg" alt="" />
+        <img class="hangman__part hangman__part--hand-right" src="assets/hand-right.svg" alt="" />
+        <img class="hangman__part hangman__part--leg-left" src="assets/leg-left.svg" alt="" />
+        <img class="hangman__part hangman__part--leg-right" src="assets/leg-right.svg" alt="" />
       </div>
     `
 
