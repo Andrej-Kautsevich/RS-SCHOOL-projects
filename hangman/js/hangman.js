dd6d2be6cb01c371a93eab2d25b23e7e4fd03fe9
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-01-15 23:14:00 +0300

    feat: add adaptive designe

--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -10,14 +10,16 @@ class Hangman {
     hangman.classList.add("hangman");
 
     hangman.innerHTML = `
-      <img class="hangman__gallows" src="assets/gallows.jpg" alt="" />
-      <div class="hangman__man">
-        <img class="hangman__part hangman__part--head" src="assets/head.svg" alt="" />
-        <img class="hangman__part hangman__part--body" src="assets/body.svg" alt="" />
-        <img class="hangman__part hangman__part--hand-left" src="assets/hand-left.svg" alt="" />
-        <img class="hangman__part hangman__part--hand-right" src="assets/hand-right.svg" alt="" />
-        <img class="hangman__part hangman__part--leg-left" src="assets/leg-left.svg" alt="" />
-        <img class="hangman__part hangman__part--leg-right" src="assets/leg-right.svg" alt="" />
+      <div class="hangman__wrapper">
+        <img class="hangman__gallows" src="assets/gallows.jpg" alt="" />
+        <div class="hangman__man">
+          <img class="hangman__part hangman__part--head" src="assets/head.svg" alt="" />
+          <img class="hangman__part hangman__part--body" src="assets/body.svg" alt="" />
+          <img class="hangman__part hangman__part--hand-left" src="assets/hand-left.svg" alt="" />
+          <img class="hangman__part hangman__part--hand-right" src="assets/hand-right.svg" alt="" />
+          <img class="hangman__part hangman__part--leg-left" src="assets/leg-left.svg" alt="" />
+          <img class="hangman__part hangman__part--leg-right" src="assets/leg-right.svg" alt="" />
+        </div>
       </div>
     `
 
