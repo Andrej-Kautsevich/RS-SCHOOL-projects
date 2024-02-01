1d365ba808cf0290f6838a5fe1e7531e9690f687
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-02-01 16:14:13 +0300

    feat: add game-buttons generation

---
+++ nonograms/src/js/components/GameButtons.js
@@ -0,0 +1,30 @@
+function createElement(tag, className, textContent) {
+  const element = document.createElement(tag);
+  if (className) element.classList.add(...className.split(' '));
+  if (textContent) element.textContent = textContent;
+  return element;
+}
+
+export default class GameButtons {
+  constructor() {
+    this.gameButtons = this.createGameButtons();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  createGameButtons() {
+    const gameButtons = createElement('div', 'game-buttons');
+
+    const solutionBtn = createElement('button', 'button game-buttons__button', 'Solution');
+    const saveBtn = createElement('button', 'button game-buttons__button', 'Save');
+    const continueBtn = createElement('button', 'button game-buttons__button', 'Continue');
+    const resetBtn = createElement('button', 'button game-buttons__button', 'Reset');
+
+    gameButtons.append(solutionBtn, saveBtn, continueBtn, resetBtn);
+
+    return gameButtons;
+  }
+
+  getGameButtons() {
+    return this.gameButtons;
+  }
+}
