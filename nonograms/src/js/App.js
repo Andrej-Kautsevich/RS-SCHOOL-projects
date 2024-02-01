eb03be19175c3732aa7aa3e4cf3f3fc983f53d06
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-02-01 16:21:02 +0300

    refactor: move page generation from index to App class method

--- nonograms/src/js/App.js
+++ nonograms/src/js/App.js
@@ -1,11 +1,21 @@
 import Cross from './Cross/Cross';
+import Timer from './components/Timer';
+import GameButtons from './components/GameButtons';
+import createMenuBar from './layout/menubar';
 
 export default class App {
-  constructor(template, timer) {
-    this.cross = new Cross(template, timer);
+  constructor(template) {
+    this.timer = new Timer();
+    this.gameButtons = new GameButtons();
+    this.cross = new Cross(template, this.timer);
   }
 
   start(container) {
-    container.appendChild(this.cross.getCross());
+    const cross = this.cross.getCross();
+    const timer = this.timer.getTimer();
+    const gameButtons = this.gameButtons.getGameButtons();
+    const menuBar = createMenuBar();
+
+    container.append(cross, timer, gameButtons, menuBar);
   }
 }
--- nonograms/src/js/index.js
+++ nonograms/src/js/index.js
@@ -1,19 +1,11 @@
 import '../sass/style.scss';
 import App from './App';
 import templates from './templates';
-import createMenuBar from './layout/menubar';
-import Timer from './components/Timer';
 
 const mainContainer = document.createElement('main');
 mainContainer.classList.add('page-wrapper');
 document.body.append(mainContainer);
 
-const timer = new Timer();
-const timerContainer = timer.getTimer();
-
-const app = new App(templates[1].template, timer);
-const menuBar = createMenuBar();
-
-mainContainer.append(menuBar, timerContainer);
+const app = new App(templates[1].template);
 
 app.start(mainContainer);
