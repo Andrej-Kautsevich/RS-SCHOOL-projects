05fec8471a2916775406e5120e7ee2c6a14be0df
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-02-24 15:42:37 +0300

    refactor: migrate news.js to TypeScript

---
+++ migration-to-ts/src/components/view/news/news.ts
@@ -0,0 +1,42 @@
+import { NewsItem, View, assertIsDefined, assertIsInstanceOf, queryElement } from '../../../types/index';
+import './news.css';
+
+class News implements View<NewsItem> {
+    draw(data: NewsItem[]): void {
+        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+        const fragment = document.createDocumentFragment();
+        const newsItemTemp = document.querySelector('#newsItemTemp');
+        assertIsInstanceOf(newsItemTemp, HTMLTemplateElement);
+
+        news.forEach((item, idx) => {
+            const newsClone = newsItemTemp.content.cloneNode(true);
+            assertIsInstanceOf(newsClone, DocumentFragment);
+            assertIsDefined(newsClone);
+
+            if (idx % 2) queryElement(newsClone, Element, '.news__item').classList.add('alt');
+
+            queryElement(newsClone, HTMLElement, '.news__meta-photo').style.backgroundImage = `url(${
+                item.urlToImage || 'img/news_placeholder.jpg'
+            })`;
+            queryElement(newsClone, HTMLElement, '.news__meta-author').textContent = item.author || item.source.name;
+            queryElement(newsClone, HTMLElement, '.news__meta-date').textContent = item.publishedAt
+                .slice(0, 10)
+                .split('-')
+                .reverse()
+                .join('-');
+
+            queryElement(newsClone, HTMLElement, '.news__description-title').textContent = item.title;
+            queryElement(newsClone, HTMLElement, '.news__description-source').textContent = item.source.name;
+            queryElement(newsClone, HTMLElement, '.news__description-content').textContent = item.description;
+            queryElement(newsClone, HTMLElement, '.news__read-more a').setAttribute('href', item.url);
+
+            fragment.append(newsClone);
+        });
+
+        queryElement(document, Element, '.news').innerHTML = '';
+        queryElement(document, Element, '.news').appendChild(fragment);
+    }
+}
+
+export default News;
--- migration-to-ts/src/types/index.ts
+++ migration-to-ts/src/types/index.ts
@@ -39,3 +39,23 @@ export function assertIsInstanceOf<T>(element: unknown, type: { new (...args: un
         throw new Error(`${element} is not an instance of ${type.name}`);
     }
 }
+
+function checkedQuerySelector(parent: Element | Document | DocumentFragment, selector: string): Element {
+    const el = parent.querySelector(selector);
+    if (!el) {
+        throw new Error(`Selector ${selector} didn't match any elements.`);
+    }
+    return el;
+}
+
+export function queryElement<T extends typeof Element>(
+    container: Element | Document | DocumentFragment,
+    type: T,
+    selector: string
+): InstanceType<T> {
+    const el = checkedQuerySelector(container, selector);
+    if (!(el instanceof type)) {
+        throw new Error(`Selector ${selector} matched ${el} which is not an ${type}`);
+    }
+    return el as InstanceType<T>;
+}
