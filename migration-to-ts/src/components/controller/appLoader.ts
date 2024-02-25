5cedc21383610155a675ae757addbe1014fee9ce
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-02-25 22:53:30 +0300

    refactor: migrate appLoader.js to TypeScript

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,6 +2,9 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
+        if (!process.env.API_URL || !process.env.API_KEY) {
+            throw new Error('API URL or KEY is not valid!');
+        }
         super(process.env.API_URL, {
             apiKey: process.env.API_KEY,
         });
