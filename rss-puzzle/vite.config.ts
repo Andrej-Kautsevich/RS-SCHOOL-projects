58c5358e174d47af892e9251449a10be65b5ec11
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2024-03-13 18:54:31 +0300

    fix: RSS-PZ-37 fix Vite config file, fix files naming (#52)
    * feat: add app creation (#51)
    
    * add app creation
    
    * fix: fix Vite config file, fix files naming

--- rss-puzzle/vite.config.ts
+++ rss-puzzle/vite.config.ts
@@ -3,4 +3,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
 });
