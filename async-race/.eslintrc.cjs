f25995837ab3870ef88fae5afcd35b86d7e0a5a3
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2024-03-27 21:19:52 +0300

    Chore: 01 init task (#115)
    * init: start task
    
    * chore: add devDependencies

---
+++ async-race/.eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: '**/tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'airbnb',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts', 'commitlint.config.js'],
+};
