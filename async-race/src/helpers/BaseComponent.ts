a42399db0acea90352bf67cde06e7738f1d3e1e5
Andrej-Kautsevich <andrej.kautsevich@yandex.by>
2024-03-29 19:09:40 +0300

    feat: 03 add DOM elements generation(#117)

---
+++ async-race/src/helpers/BaseComponent.ts
@@ -0,0 +1,138 @@
+import isNotNullable from './isNotNullable';
+
+export type Props<T extends HTMLElement = HTMLElement> = Partial<
+  Omit<T, 'style' | 'dataset' | 'classList' | 'children' | 'tagName'>
+> & {
+  txt?: string;
+  tag?: keyof HTMLElementTagNameMap;
+  classNames?: string[];
+};
+
+export type ElementFnProps<T extends HTMLElement = HTMLElement> = Omit<Props<T>, 'tag'>;
+
+export class BaseComponent<T extends HTMLElement = HTMLElement> {
+  protected node: T;
+
+  protected children: BaseComponent[] = [];
+
+  constructor(props: Props<T>, ...children: (BaseComponent | HTMLElement)[]) {
+    const node = document.createElement(props.tag ?? 'div') as T;
+    // Assign properties from props to the node
+    Object.assign(node, props);
+
+    if (props.txt) {
+      node.textContent = props.txt;
+    }
+    if (props.classNames) {
+      node.classList.add(...props.classNames);
+    }
+    this.node = node;
+
+    if (children) {
+      this.appendChildren(children);
+    }
+  }
+
+  /**
+   * Appends a child component to the current component.
+   */
+  public append(child: BaseComponent | HTMLElement): void {
+    if (child instanceof BaseComponent) {
+      this.children.push(child);
+      this.node.append(child.getNode());
+    } else {
+      this.node.append(child);
+    }
+  }
+
+  public appendChildren(children: (BaseComponent | HTMLElement | null)[]): void {
+    children.filter(isNotNullable).forEach((el) => {
+      this.append(el);
+    });
+  }
+
+  /**
+   * Returns the HTML node associated with the component.
+   */
+  public getNode() {
+    return this.node;
+  }
+
+  /**
+   * Returns an array of child components.
+   */
+  public getChildren() {
+    return this.children;
+  }
+
+  /**
+   * Sets the text content of the component.
+   */
+  public setTextContent(content: string): void {
+    this.node.textContent = content;
+  }
+
+  /**
+   * Sets an attribute on the component's HTML node.
+   */
+  public setAttribute(attribute: string, value: string): void {
+    this.node.setAttribute(attribute, value);
+  }
+
+  /**
+   * Removes an attribute from the component's HTML node.
+   */
+  public removeAttribute(attribute: string): void {
+    this.node.removeAttribute(attribute);
+  }
+
+  /**
+   * Toggles the presence of a CSS class on the component's HTML node.
+   * @param {boolean} force - If force is not given, "toggles" token, removing it if it's present and adding it if it's not present. If force is true, adds token (same as add()). If force is false, removes token (same as remove()).
+   */
+  public toggleClass(className: string, force?: boolean): void {
+    this.node.classList.toggle(className, force);
+  }
+
+  /**
+   * Add one or more CSS classes on the component's HTML node.
+   * @param {string[]} classNames - array of classes names
+   */
+  public addClasses(classNames: string[]): void {
+    this.node.classList.add(...classNames);
+  }
+
+  /**
+   * Remove one or more CSS classes on the component's HTML node.
+   * @param {string[]} classNames - array of classes names
+   */
+  public removeClasses(classNames: string[]): void {
+    this.node.classList.remove(...classNames);
+  }
+
+  public addListener(event: string, listener: EventListener, options = false): void {
+    this.node.addEventListener(event, listener, options);
+  }
+
+  public removeListener(event: string, listener: EventListener, options = false): void {
+    this.node.removeEventListener(event, listener, options);
+  }
+
+  /**
+   * Destroys all child components associated with the current component.
+   */
+  destroyChildren() {
+    this.children.forEach((child) => {
+      child.destroy();
+    });
+    this.children.length = 0;
+  }
+
+  /**
+   * Destroys the current component and removes its HTML node from the DOM.
+   */
+  destroy() {
+    this.destroyChildren();
+    this.node.remove();
+  }
+}
---
+++ async-race/src/helpers/tags.ts
@@ -0,0 +1,53 @@
+import { BaseComponent, ElementFnProps } from './BaseComponent';
+
+export const div = (props: ElementFnProps, ...children: (BaseComponent | HTMLElement)[]) => {
+  return new BaseComponent<HTMLDivElement>({ ...props }, ...children);
+};
+
+export const p = (props: ElementFnProps, ...children: BaseComponent[]) => {
+  return new BaseComponent<HTMLParagraphElement>({ ...props, tag: 'p' }, ...children);
+};
+
+export const span = (props: ElementFnProps) => {
+  return new BaseComponent<HTMLSpanElement>({ ...props, tag: 'span' });
+};
+
+export const main = (props: ElementFnProps, ...children: BaseComponent[]) => {
+  return new BaseComponent<HTMLElementTagNameMap['main']>({ ...props, tag: 'main' }, ...children);
+};
+
+export const form = (props: ElementFnProps, ...children: BaseComponent[]) => {
+  return new BaseComponent<HTMLFormElement>({ ...props, tag: 'form' }, ...children);
+};
+
+export const label = (props: ElementFnProps<HTMLLabelElement>, ...children: (BaseComponent | HTMLElement)[]) => {
+  return new BaseComponent<HTMLLabelElement>({ ...props, tag: 'label' }, ...children);
+};
+
+export const input = (props: ElementFnProps<HTMLInputElement>) => {
+  return new BaseComponent<HTMLInputElement>({ ...props, tag: 'input' });
+};
+
+export const select = (props: ElementFnProps<HTMLSelectElement>, ...children: (BaseComponent | HTMLElement)[]) => {
+  return new BaseComponent<HTMLSelectElement>({ ...props, tag: 'select' }, ...children);
+};
+
+export const option = (props: ElementFnProps<HTMLOptionElement>) => {
+  return new BaseComponent<HTMLOptionElement>({ ...props, tag: 'option' });
+};
+
+export const button = (props: ElementFnProps<HTMLButtonElement>, ...children: (BaseComponent | HTMLElement)[]) => {
+  return new BaseComponent<HTMLButtonElement>({ ...props, tag: 'button' }, ...children);
+};
+
+type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
+export const h = (level: HeadingLevel, props: ElementFnProps, ...children: BaseComponent[]) => {
+  return new BaseComponent<HTMLHeadingElement>(
+    { ...props, tag: `h${level}` as keyof HTMLElementTagNameMap },
+    ...children,
+  );
+};
+
+export const img = (source: string, props?: ElementFnProps<HTMLImageElement>) => {
+  return new BaseComponent<HTMLImageElement>({ ...props, src: source, tag: 'img' });
+};
